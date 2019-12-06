a2886ab356d2177d108f1eab2e60497ef25d7dda
almagro <emazzurque@gmail.com>
2019-12-06 00:26:38 -0300

    html y css funcando

--- apitax/urls.py
+++ apitax/urls.py
@@ -2,12 +2,15 @@
 
 from django.contrib import admin
 from django.urls import path
-from core import views as core_views
+from core import views 
 from django.conf import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
    
-    path('home/', core_views.home, name='home'),
+    path('',views.home, name='home'),
+    path('ingreso/',views.ingreso, name='ingreso'),
+    path('gastos/',views.gastos, name='gastos'),
+    path('movimientos/',views.movimientos, name='movimientos'),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, "core/home.html" )
+
+def ingreso(request):
+	return render(request,"core/ingreso.html")     
+
+def gastos(request):
+	return render(request,"core/gastos.html")	
+
+def movimientos(request):
+	return render(request,"core/movimientos.html")		
+
