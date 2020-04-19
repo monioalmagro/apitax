8a7a3463c87c903a3ec3ddf7e95e415b1a8660fe
emi <emazzurque@gmail.com>
2020-04-19 02:50:51 -0300

    models.py

--- ingreso/models.py
+++ ingreso/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.utils.timezone import now
 
 class Ingreso(models.Model):
-    monto=models.CharField(verbose_name="monto" ,max_length=50)
-
+    #monto=models.CharField(verbose_name="monto" ,max_length=50)
+    monto=models.IntegerField(verbose_name="monto")
+    descripcion=models.CharField(null=True,verbose_name="descripcion" ,max_length=200)
+    fecha= models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     
 
     class Meta:
