176d55d32744673c3c3a17e1a02d0904e58a4c85
emi <emazzurque@gmail.com>
2020-04-19 23:56:36 -0300

    modificacion database

--- ingreso/models.py
+++ ingreso/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 
 class Ingreso(models.Model):
-    #monto=models.CharField(verbose_name="monto" ,max_length=50)
+    
     monto=models.IntegerField(verbose_name="monto")
     descripcion=models.CharField(verbose_name="descripcion" ,max_length=200)
     fecha= models.DateTimeField( verbose_name='Fecha de creación')
@@ -15,4 +15,37 @@ class Ingreso(models.Model):
     def __str__(self):
         return self.descripcion
 
+class Categoria(models.Model):
+    cuenta=models.CharField(verbose_name="cuenta", max_length=50)
+    categoria=models.CharField(verbose_name="categoria", max_length=50)
+    signo=models.CharField(verbose_name="signo", max_length=3)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __str__(self):
+        return self.categoria
+
+class Movimiento(models.Model):
+    cuenta =models.CharField("cuenta", max_length=50)
+    #categoria=models.CharField(verbose_name="categoria", max_length=50)
+    cantidad=models.IntegerField("cantidad")
+    signo=models.CharField("signo", max_length=50)
+    detalle=models.CharField("detalle", max_length=50)
+    models.DateTimeField("fecha", auto_now=True, auto_now_add=True)
+
     
+
+    class Meta:
+        verbose_name = "Movimiento"
+        verbose_name_plural = "Movimientos"
+
+    def __str__(self):
+        return self.movimiento
+
+        
+
+   
+
+   
